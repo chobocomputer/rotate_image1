878018f74dea16f9eb59e2a1443d3b51b57231b0
chobocomputer <51562730+chobocomputer@users.noreply.github.com>
2019-11-03 12:43:35 +0900

    Add files via upload

---
+++ giuri_270.py
@@ -0,0 +1,54 @@
+import os, datetime, time
+
+from PIL import Image
+
+
+
+################## make direc #########################################
+def makedirec():
+    global naljja, naldirec
+    
+    naljja = datetime.datetime.now()
+    naldirec = naljja.strftime('%Y%m%d_%H%M%S')
+    os.makedirs(naldirec)
+#######################################################################
+
+
+
+################## rotate_270       ####################################
+def rotate_270():
+        original_image = Image.open(filename)
+
+        original_image = original_image.rotate(360-270, expand = True)
+
+        file_front_name, file_back_name = os.path.splitext(filename)
+
+        original_image.save(os.path.join(naldirec, file_front_name + '.png'))
+
+        original_image.close()   
+#######################################################################
+
+
+
+################## exist file #########################################
+def exist_file():
+    if (filename.lower().endswith('.png') or filename.lower().endswith('.jpg') or filename.lower().endswith('.gif') or filename.lower().endswith('.bmp') or filename.lower().endswith('.pcx')):
+        return True
+    
+
+#######################################################################
+
+
+################## main       #########################################
+
+naljja = 0
+naldirec = 0
+makedirec()
+
+
+
+for filename in os.listdir('.'):
+    if exist_file():    
+        rotate_270()
+
+#######################################################################
